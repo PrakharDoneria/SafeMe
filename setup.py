eb532b8351f84fc2c3436968699525283ffc2ceb
prakhardoneria <gamesprotec@gmail.com>
2025-07-08 01:59:18 +0530

    PyPi Fix

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the long description from README.md
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="SafeMe",
+    version="0.1.0",
+    description="One-command backend Linux server security: firewall, SSH hardening, DDoS protection, integrity checks and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Prakhar Doneria",
+    author_email="prakhardoneria3@gmail.com",
+    url="https://github.com/prakhardoneria/safeme",
+    project_urls={
+        "Homepage": "https://github.com/prakhardoneria/safeme",
+        "Repository": "https://github.com/prakhardoneria/safeme",
+        "Issues": "https://github.com/prakhardoneria/safeme/issues",
+    },
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=[
+        "typer[all]",
+    ],
+    entry_points={
+        "console_scripts": [
+            "safeme = safeme.cli:app",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+        "Topic :: Security",
+        "Topic :: System :: Systems Administration",
+        "Environment :: Console",
+    ],
+)
