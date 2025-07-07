3d623373a8d5eb580b4f81c2a3c939bf82dce9c1
prakhardoneria <gamesprotec@gmail.com>
2025-07-08 02:18:43 +0530

    PyPi Fix

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
+        "Operating System :: POSIX :: Linux",
     ],
 )
